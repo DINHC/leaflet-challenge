421b9dc97d72ca51e8a7a4288dd0f8fdfe38b861
Dinh Chau <dinhchau001@gmail.com>
2021-08-30 16:46:54 -0700

    trying to work on overlay

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -15,6 +15,8 @@ var map = L.map('map', {
     zoom: 13
 });
 
+
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18, 
@@ -33,7 +35,7 @@ d3.json(url, function(data) {
   //         radius: feature.properties.mag * 4
   //     };
   //   }
-    
+
     function styleInfo(feature) {
       return {
         opacity: 1,
@@ -44,4 +46,33 @@ d3.json(url, function(data) {
         stroke: true,
         weight: 0.5
       };
-    }})
+    }
+    var colors = ["#FFBE33", "#FFA833", "#FF9333", "#FF5833", "#FF4933","#FF3333"]
+    function getColor(magnitude){
+    if (magnitude < 1){
+      color = colors[0];
+    } else if (magnitude >= 1 && magnitude < 2){
+      color = colors[1];
+    } else if (magnitude >= 2 && magnitude < 3){
+      color = colors[2];
+    } else if (magnitude >= 3 && magnitude < 4){
+      color = colors[3];
+    } else if (magnitude >= 4 && magnitude < 5){
+      color = colors[4];
+    } else {
+      color = colors[5];
+    }
+    return color;}})
+  
+    // var features = data["features"]
+    // for (var i = 0; i < features.length; i++) {
+    //     var geometry = features[i]["geometry"]["coordinates"];
+    //     var magnitude = features[i]["properties"]["mag"];
+    //     var title = features[i]["properties"]["title"];
+    //     var coords = {
+    //       longitude: geometry["0"],
+    //       latitude: geometry["1"]
+    //     };
+
+      
+      
