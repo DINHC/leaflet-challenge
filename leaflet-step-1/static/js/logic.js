6c38e2ad1d8167d0828e26b28aa6ee43066b577a
Dinh Chau <dinhchau001@gmail.com>
2021-08-30 17:50:31 -0700

    Still working on overlaying on static map

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -8,34 +8,12 @@
 //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
 //     .openPopup();
 
-var url =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
-
 var map = L.map('map', {
     center: [34.03, -118.14], 
     zoom: 13
 });
-
-
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 18, 
-}).addTo(map);
-
-d3.json(url, function(data) {
-    function styleInfo(feature) {
-      return {
-        opacity: 1,
-        fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
-        color: "#000000",
-        radius: getRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.5
-      };
-    }
-    var colors = ["#FFBE33", "#FFA833", "#FF9333", "#FF5833", "#FF4933","#FF3333"]
-    function getColor(magnitude){
+var colors = ["#FF0000", "#FF6900", "#FFC100", "#E5FF00", "#8DFF00","#00FF00"]
+    function getColor(magnitude){{
     if (magnitude < 1){
       color = colors[0];
     } else if (magnitude >= 1 && magnitude < 2){
@@ -49,17 +27,41 @@ d3.json(url, function(data) {
     } else {
       color = colors[5];
     }
-    return color;}})
-  
-    // var features = data["features"]
-    // for (var i = 0; i < features.length; i++) {
-    //     var geometry = features[i]["geometry"]["coordinates"];
-    //     var magnitude = features[i]["properties"]["mag"];
-    //     var title = features[i]["properties"]["title"];
-    //     var coords = {
-    //       longitude: geometry["0"],
-    //       latitude: geometry["1"]
-    //     };
+    return colors;}}
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18, 
+}).addTo(map);
+var url =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+d3.json(url, function(data) {
+  var features = data["features"];
+  for (var i = 0; i < features.length; i++) {
+    var geometry = features[i]["geometry"]["coordinates"];
+    var magnitude = features[i]["properties"]["mag"];
+    var title = features[i]["properties"]["title"];
+    var coords = {
+      longitude: geometry["0"],
+      latitude: geometry["1"]
+    };
+  
+  
+  
+  // function styleInfo(feature) {
+    //   return {
+    //     opacity: 1,
+    //     fillOpacity: 1,
+    //     fillColor: getColor(feature.properties.mag),
+    //     color: "#000000",
+    //     radius: getRadius(feature.properties.mag),
+    //     stroke: true,
+    //     weight: 0.5
+    //   };
+    // }
+
+
+   
+
+    
       
-      
+  }})
